2d6b37fbf042d24cd3b99c1a8ce113c51b5a9949
avoleon15 <139786623+avoleon15@users.noreply.github.com>
2023-10-06 11:53:15 -0600

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Empleado {
+    private List<Cliente> listaCliente;
+
+    public Empleado(){
+        listaCliente = new ArrayList<>();
+    }
+
+    public void registrarCliente(Cliente cliente){
+        listaCliente.add(cliente);
+    }
+
+    public void mostrarClientes(){
+        System.out.println("Clientes: ");
+        for (Cliente cliente : listaClientes){
+            System.out.println("Nombre: " + cliente.getNombre() + "Edad: " + cliente.getEdad() + "DPI: " + cliente.getDPI());
+        }
+    }
+}
