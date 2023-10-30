06da7f2274953ee4f7fbf0e72d1d3a5aa9659dbc
DerNait <75186178+DerNait@users.noreply.github.com>
2023-10-29 18:55:49 -0600

    Driver prueba de GUI

--- GymDriver.java
+++ GymDriver.java
@@ -1,12 +1,34 @@
-/* Proyecto#2 POO
- * Autores: Marinés García 23391, Nery Molina 23218, Kevin Villagrán 23584, Álvaro León 23274
- * MAIN
- */
+import javax.swing.*;
+import java.awt.*;
 
-public class GymDriver{
+public class GymDriver extends JFrame{
 
-    public static void main(String[] args){
+    final private Font mainFont = new Font("Arial", Font.BOLD, 18);
+    JLabel lbBienvenida; 
+    JTextField fieldTest;
 
+    public void initialize(){
+
+        /*******BIENVENIDA*******/
+        lbBienvenida = new JLabel("ULTRAFIT", SwingConstants.CENTER);
+        lbBienvenida.setFont(mainFont);
+
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(lbBienvenida);
+
+        add(mainPanel, BorderLayout.NORTH);
+
+        setTitle("ULTRAFIT");
+        setSize(600,600);
+        setMinimumSize(new Dimension(300, 300));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
+    public static void main(String[] main){
+        GymDriver myFrame = new GymDriver();
+        myFrame.initialize();
+    }
 }
