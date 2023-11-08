8c8b398365c0cc2f07ef4e6a4563e9933ece11db
NESHGP04 <89556682+NESHGP04@users.noreply.github.com>
2023-11-08 10:30:36 -0600

    Clase Rutina actualizado

--- Rutina.java
+++ Rutina.java
@@ -5,25 +5,54 @@
 
 import java.util.*;
 
-public class Rutina{
+class Rutina{
+
     private ArrayList<Secuencia> secuencias = new ArrayList<Secuencia>();
-    private String[] dias = new String[6];
+    //private String[] dias = new String[6];
+    private String dias;
     private ArrayList<Maquina> maquinas = new ArrayList<Maquina>();
     private int tipo;
+    private ArrayList<String> rutinas;
 
-    Rutina(int tipo, String ... dias){
+    Rutina(int tipo, String dias){
         this.tipo = tipo;
+        this.dias = dias;
+        this.rutinas = new ArrayList<>();
     }
 
     public ArrayList<Secuencia> getSecuencias(){
         return secuencias;
     }
 
-    public String[] getDias(){
-        return dias;
+    public ArrayList<Maquina> getMaquina(){
+        return maquinas;
     }
 
-    public void setDias(String ... dias){
-        this.dias = dias;
+    // public String[] getDias(){
+    //     return dias;
+    // }
+
+    // public void setDias(String ... dias){
+    //     this.dias = dias;
+    // }
+
+    public void agregarRutina(String rutina){
+        rutinas.add(rutina);
+    }
+
+    public String toCSV() {
+        StringBuilder csvData = new StringBuilder();
+        csvData.append(tipo).append(",").append(dias);
+
+        for (int i = 0; i < 5; i++) {
+            if (i < rutinas.size()) {
+                csvData.append(",").append(rutinas.get(i));
+            } else {
+                csvData.append(",");
+            }
+        }
+
+        csvData.append("\n");
+        return csvData.toString();
     }
 }
